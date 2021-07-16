594fc40df610d7266cfae95364ed6ec0f274f766
ravsamy <syedahmedsamy@live.com>
2021-07-16 22:58:51 +0530

    skeleton path

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,7 @@
 import { createClient } from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Image from 'next/image'
-import Skeleton from '../../components/Skeleton'
+import Skeleton from '../skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
